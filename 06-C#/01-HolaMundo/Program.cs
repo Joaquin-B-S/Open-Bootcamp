0da1606cc1570857285dac75401106ee07e64717
Joaquin Baeza <joaquin.kappa@hotmail.com>
2022-12-04 22:16:41 -0300

    curso C#

---
+++ 06-C#/01-HolaMundo/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Introduce tu nombre: ");
+string nombre = Console.ReadLine();
+Console.WriteLine("Hola, " + nombre);
+
+// edad
+Console.WriteLine("Introduce tu edad: ");
+int edad = Int32.Parse(Console.ReadLine());
+Console.WriteLine("Hola, " + edad);
---
+++ 06-C#/tarea1/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Escribe tú nombre: ");
+string nombre = Console.ReadLine();
+Console.WriteLine("Hola " + nombre);
+
+Console.WriteLine("Hora: ");
+string hora = Console.ReadLine();
+Console.WriteLine("La hora es " + hora);
